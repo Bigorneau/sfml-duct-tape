2e83375ffc67e3af4d7e107e9e1fc93318257c51
Patrice Blin <dev@patriceblin.com>
2014-05-15 22:44:04 +0200

    keyboard: remove thread

--- keyboard/point/point.cpp
+++ keyboard/point/point.cpp
@@ -1,10 +1,6 @@
-#include <unistd.h>
-
 #include <iostream>
 
 #include <SFML/Graphics.hpp>
-
-#include <X11/Xlib.h>
 
 #define HEIGHT 800
 #define WIDTH 600
@@ -30,60 +26,52 @@ int move(int x, int y)
     return 0;
 }
 
-int readKeyboard(int delta)
+int readKeyboard()
 {
     int x, y;
-    while (isRunning) {
-        x = 0;
-        y = 0;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            isRunning = false;
-            break;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            x = 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            x = -1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            y = 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            y = -1;
-        }
-        if (x != 0 || y != 0)
-            move(x, y);
-        usleep(200);
+    x = 0;
+    y = 0;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+        return -1;
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        x = 1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        x = -1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        y = 1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        y = -1;
+    }
+    if (x != 0 || y != 0)
+        move(x, y);
     return 0;
 }
 
 int main()
 {
-    XInitThreads();
-
     sf::RenderWindow window(sf::VideoMode(HEIGHT, WIDTH), "point window");
     sf::Event event;
     sf::CircleShape circle(10.f);
-    sf::Thread keyboardThread(&readKeyboard, 0);
 
     window.setActive(false);
     circle.setFillColor(sf::Color::Red);
     circle.setPosition(point.x, point.y);
-    keyboardThread.launch();
 
     while (isRunning) {
         window.clear(sf::Color::Black);
+
+        if (readKeyboard() < 0)
+            break;
 
         circle.setPosition(point.x, point.y);
 
         window.draw(circle);
         window.display();
     }
-
-    keyboardThread.terminate();
-    keyboardThread.wait();
 
     return 0;
 }
